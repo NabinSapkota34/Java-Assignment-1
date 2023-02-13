4b1f69446c012cb169af9f01784d153c94711131
Nabin Sapkota <70211071+NabinSapkota34@users.noreply.github.com>
2023-02-13 17:06:06 +0545

    Add files via upload

---
+++ Nabin.java
@@ -0,0 +1,16 @@
+public class Nabin {
+   public static void main(String[] args) {
+      System.out.println("Name of the Organization: Kamalpokhari Resturant & Bar\n\n");
+      System.out.println("Address:");
+      System.out.println("----State: KM");
+      System.out.println("----Zip Code: 44600");
+      System.out.println("----Country: Nepal\n\n");
+      System.out.println("Contact Information:");
+      System.out.println("----Mobile Number: 9865382489");
+      System.out.println("----Email: kamalpokhari.rnb@gmail.com");
+
+
+
+
+    }
+}
